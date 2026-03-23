77c88fd97b6e306b1fd1d8e9c7e130020a104337
JUNSU JEONG <omoroie@naver.com>
2026-03-23 23:13:53 +0900

    누가 만든거야 이딴문제

---
+++ std_java/exam7.java
@@ -0,0 +1,14 @@
+// 쓰레기 노가다 문제2
+package std_java;
+
+public class exam7 {
+    public static void main(String[] args) {
+        int[] data = {3, 5, 8, 12, 17};
+        System.out.println(func(data, 0, data.length - 1));
+    }
+    static int func(int[] a, int st, int end) {
+        if (st >= end) return 0;
+        int mid = (st + end) / 2;
+        return a[mid] + Math.max(func(a, st, mid), func(a, mid + 1, end));
+    }
+}
