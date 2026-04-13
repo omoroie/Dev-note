3c23a816b4dc27cc6ea9e06e402c65416aa7ddc6
Omoroie <omoroie@naver.com>
2026-04-13 10:18:06 +0000

    std

---
+++ std_java/exam10.java
@@ -0,0 +1,23 @@
+package std_java;
+
+public class exam10 {
+    public static void main(String[] args) {
+        Level lv = Level.values()[Level.X.name().length()];
+        System.out.println(lv.getTag());
+    }
+
+}
+
+enum Level {
+    X("A"), Y("AB"), Z("ABC");
+
+    private String tag;
+
+    Level(String tag) {
+        this.tag = tag;
+    }
+    
+    public String getTag() {
+        return tag;
+    }
+}
